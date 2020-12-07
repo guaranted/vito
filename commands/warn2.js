d30dc13e9c3b9bc3f673d28c4a89ec5dbc54fd53
guaranted <73685531+guaranted@users.noreply.github.com>
2020-12-07 12:05:09 +0300

    Create warn2.js

---
+++ commands/warn2.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'warn2',
+    description: 'command to mute members',
+    execute(message, args){
+        if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply(`Bu komutu kullanabilmek için **Üyeleri Yasakla** iznine sahip olmalısın!`);
+             const  role = message.guild.roles.cache.find(r => r.name === "Uyarı 2");
+            if(!role) return message.reply('Lütfen \`Uyarı 2\` adında bir rol oluşturun!')
+      
+           const member = message.mentions.members.first();
+      if(member.id === "771674804909637632") return message.reply('Beni uyaramazsın :)')
+      if(member.id === message.author.id) return message.reply('Kendini uyaramazsın!')
+      if(member.id === "432150936534646805") return message.reply('Sahibi uyaramazsın.')
+            if (!member) return message.reply('Birini etiketle')
+            member.roles.add(role).then(newMember => {
+                message.channel.send(`${member} üyesinin **2**. uyarısı verildi! ` )
+            })
+    }
+}
